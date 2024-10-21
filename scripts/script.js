6e4b385218dede23bcd123fdef4aeb6f43fa6eb5
temaantonov11 <temaantonov11@yandex.ru>
2024-10-21 14:55:03 +0300

    +2 currency

--- scripts/script.js
+++ scripts/script.js
@@ -31,6 +31,12 @@ document.getElementById("change-currency").onclick = function(e) {
     } else if (currentCurrency === "₽") {
         newCurrency = "BYN";
         coefficient = 3;
+    } else if (currentCurrency === "BYN") {
+        newCurrency = "€";
+        coefficient = 0.9;
+    } else if (currentCurrency === "€") {
+        newCurrency = "¥";
+        coefficient = 6.9;
     }
 
     e.target.innerText = newCurrency;
